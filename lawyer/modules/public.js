910b7fa3c5510cefa9ba90f7ecb9ae47eee0e8eb
weianzi <ancool@foxmail.com>
2015-02-17 17:04:24 +0800

    25

--- lawyer/modules/public.js
+++ lawyer/modules/public.js
@@ -14,13 +14,33 @@ define(function (require, exports, module) {
 	//苹果CSS无hover
 	function fnHover( myLinks ){
 		for(var i = 0; i < myLinks.length; i++){
-			myLinks.bind("touchstart", function(){
+//			myLinks.bind("touchstart", function(){
+//				$(this).addClass("hover");
+//			}).bind("touchend", function(){
+//				$(this).removeClass("hover");
+//			});	
+			myLinks.bind("click", function(){
 				$(this).addClass("hover");
-			}).bind("touchend", function(){
-				$(this).removeClass("hover");
 			});	
 		}
 	}
 	fnHover( $(".myLinks") );
+	
+	//回复详情 切换
+	$(".lawyer-info .tab li").each(function(index) {
+		$(this).click(function(){
+			$(this).addClass("selected").siblings().removeClass("selected");
+			$(".lawyer-info .tab-con").hide().eq(index).show();
+		});
+    });
+
+	$(".msn-list .tab > div").each(function(index) {
+		$(this).click(function(){
+			$(this).addClass("selected").siblings().removeClass("selected");
+			$(".msn-list .tab-con").hide().eq(index).show();
+		});
+    });
+
+
 
 });
