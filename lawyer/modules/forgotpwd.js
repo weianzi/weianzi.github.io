a8f8d6269c9d6e3bc8a4ff28b6a5f9996f31b755
weianzi <ancool@foxmail.com>
2015-03-12 14:06:15 +0800

    s27

--- lawyer/modules/forgotpwd.js
+++ lawyer/modules/forgotpwd.js
@@ -1,10 +1,25 @@
+$.extend(validateFunction, {
+    FORM_validate: function () {
+        $("#username").jdValidate(validatePrompt.username, validateFunction.username, true);
+        $("#pwd").jdValidate(validatePrompt.pwd, validateFunction.pwd, true);
+        $("#pwd2").jdValidate(validatePrompt.pwd2, validateFunction.pwd2, true);
+        return validateFunction.FORM_submit(["#pwd", "#pwd2"]);
+    }
+});
+
 $(function () {
+    //用户名验证
+    $("#username").jdValidate(validatePrompt.username, validateFunction.username);
+    //密码验证
+    $("#pwd").bind("keyup", function () {
+    }).jdValidate(validatePrompt.pwd, validateFunction.pwd);
+    //二次密码验证
+    $("#pwd2").jdValidate(validatePrompt.pwd2, validateFunction.pwd2);
 
     //表单提交验证和服务器请求
     $("#btnForgotpwd").tap(function () {
         var flag = validateFunction.FORM_validate();
         if (flag) {
-            //$(this).attr({"disabled": "disabled"}).attr({"value": "提交中,请稍等"});
             $.ajax({
                 type: "POST",
                 url: "#",
