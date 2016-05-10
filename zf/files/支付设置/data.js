e69540a8f7040faab72b13407c488570caa9a76c
ancool <ancool@foxmail.com>
2016-05-10 11:58:08 +0800

    del

--- zf/files/支付设置/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n,o],p,_(q,r,s,t,u,v,w,_(),x,_(y,z,A,B,C,_(D,E,F,G),H,null,I,B,J,B,K,L,M,null,N,O,P,Q,R,S,T,O),U,_(),V,_(),W,_(X,[])),Y,_(),Z,_());}; 
-var b="url",c="支付设置.html",d="generationDate",e=new Date(1440479381122.6),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="SafetyState",k="IdentityAuthentic",l="switchon",m="Logged",n="HomeState",o="Account",p="page",q="packageId",r="04f6481aa85141ca8c5bc94916646619",s="type",t="Axure:Page",u="name",v="支付设置",w="notes",x="style",y="baseStyle",z="627587b6038d43cca051c114ac41ad32",A="pageAlignment",B="near",C="fill",D="fillType",E="solid",F="color",G=0xFFFFFFFF,H="image",I="imageHorizontalAlignment",J="imageVerticalAlignment",K="imageRepeat",L="auto",M="favicon",N="sketchFactor",O="0",P="colorStyle",Q="appliedColor",R="fontName",S="Applied Font",T="borderWidth",U="adaptiveStyles",V="interactionMap",W="diagram",X="objects",Y="masters",Z="objectPaths";
-return _creator();
-})());
